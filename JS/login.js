e70979bcb6b0ef438438ac61c5a95a24cdba9192
Pragya Gupta <pragya.gupta012@gmail.com>
2023-02-09 00:22:42 +0530

    form validation

---
+++ JS/login.js
@@ -0,0 +1,16 @@
+function seterror(id,error){
+    element = document.getElementById(id);
+    element.getElementsByClassName();
+}
+function validateform()
+{
+    var returnval=true; //this will make sure the form submits only when the returnval is true.
+    var contact = document.forms['loginform']['contact'].value
+    if(contact.length<10)
+    {
+       seterror('name','Please enter a valid phone number');
+       returnval = false;
+    }
+    return returnval;
+
+}
