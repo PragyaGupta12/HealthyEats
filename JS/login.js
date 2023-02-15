7a13d1ddcb8dc703768df97482dfed8eed9ebcc6
Pragya Gupta <pragya.gupta012@gmail.com>
2023-02-15 23:37:03 +0530

    modified

--- JS/login.js
+++ JS/login.js
@@ -1,16 +1,25 @@
-function seterror(id,error){
-    element = document.getElementById(id);
-    element.getElementsByClassName('ferror')[0].innerHTML=error;
-}
-function validateform()
-{
-    var returnval=true; //this will make sure the form submits only when the returnval is true.
-    var contact = document.forms['loginform']['contact'].value
-    if(contact.length<10)
-    {
-       seterror('name','Please enter a valid phone number');
-       returnval = false;
-    }
-    return returnval;
+// function seterror(id,error){
+//     element = document.getElementById(id);
+//     element.getElementsByClassName('ferror')[0].innerHTML=error;
+// }
+// function validateform()
+// {
+//     var returnval=true; //this will make sure the form submits only when the returnval is true.
+//     var contact = document.forms['loginform']['contact'].value
+//     if(contact.length<10)
+//     {
+//        seterror('name','Please enter a valid phone number');
+//        returnval = false;
+//     }
+//     return returnval;
 
+// }
+// document.getElementById("form-btn").onclick = function login() {
+//     location.href = "afterlogin.html"
+// }
+
+function login()
+{
+var url = "www.google.com";
+window.location.href=url;
 }
