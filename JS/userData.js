4c13d7356cf3b6fd015a09b5e7b42ebe23ee4815
Pragya Gupta <pragya.gupta012@gmail.com>
2023-03-26 22:42:00 +0530

    modified

--- JS/userData.js
+++ JS/userData.js
@@ -1,8 +1,12 @@
 function login()
 {
-    var x = document.forms['loginform']['username'].value;
-    console.log("Username:", x);
+    let x = document.forms['loginform']['username'].value;
     sessionStorage.setItem("Username:", x);
+}
+function signin()
+{
+    let y = document.forms['signinform']['uname'].value;
+    sessionStorage.setItem("Username:", y);
 }
 
 
