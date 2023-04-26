827b043f548c6ddf6b18a90c04e8426b674b00b6
Pragya Gupta <pragya.gupta012@gmail.com>
2023-04-26 23:43:14 +0530

    modified

--- JS/home.js
+++ JS/home.js
@@ -1,7 +1,6 @@
-import {login} from './userData.js'
+// import {login} from './userData.js'
 
 // var val = sessionStorage.getItem("Username:"); 
 // document.getElementById('user').innerHTML = val;
 // // document.write(`<div id="user1">${val}</div>`);
-const heading = document.getElementById("user")
-const val=heading.innerHTML=sessionStorage.getItem("Username:")
+document.getElementById("user").innerHTML=sessionStorage.getItem("Username")
--- JS/userData.js
+++ JS/userData.js
@@ -1,12 +1,12 @@
 function login()
 {
     let x = document.forms['loginform']['username'].value;
-    sessionStorage.setItem("Username:", x);
+    sessionStorage.setItem("Username", x);
 }
-function signin()
-{
-    let y = document.forms['signinform']['uname'].value;
-    sessionStorage.setItem("Username:", y);
-}
+// function signin()
+// {
+//     let y = document.forms['signinform']['uname'].value;
+//     sessionStorage.setItem("Username", y);
+// }
 
-export {login}
+// export {login}
