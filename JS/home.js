2a9a0787dd2bf1cf46be95dd2dad7fbff0c8a0b0
Pragya Gupta <pragya.gupta012@gmail.com>
2023-05-22 01:29:38 +0530

    modified

--- JS/home.js
+++ JS/home.js
@@ -1,7 +1,7 @@
 // import {login} from './userData.js'
 
 // // document.write(`<div id="user1">${val}</div>`);
-let val=sessionStorage.getItem("Username")
-const val1=document.getElementById("user")
-val1.innerHTML=val
-val1.appendChild(val)
+let getVal=sessionStorage.getItem("Username")
+const val=document.getElementById("user")
+val.innerHTML=getVal
+val.appendChild(getVal)
