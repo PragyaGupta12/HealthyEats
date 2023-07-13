19efe95695cfab1263950b075453fa2a925b9852
Pragya Gupta <pragya.gupta012@gmail.com>
2023-07-13 23:43:23 +0530

    modified

--- JS/userData.js
+++ JS/userData.js
@@ -1,8 +1,21 @@
-function login()
-{
-    let username = document.forms['loginform']['username'].value;
-    sessionStorage.setItem("Username", username);
-    // let getVal=sessionStorage.getItem("Username");
-    // return document.getElementById("user").innerHTML=getVal;
-}
+window.onload = function () {
 
+    const loginForm = document.getElementById("login-form");
+    const loginButton = document.getElementById("form-btn");
+    // const loginErrorMsg = document.getElementById("login-error-msg");
+    
+    loginButton.addEventListener("click", (e) => {
+
+        // e.preventDefault();
+        const name = loginForm.username.value;
+        const pass = loginForm.password.value;
+        localStorage.setItem("username", name);
+        localStorage.setItem("password", pass);
+
+        // if (name === sessionStorage.getItem("username", name) && pass === sessionStorage.getItem("password", pass)) {
+        //     alert("You have successfully logged in.");
+        // } // else {
+        //     loginErrorMsg.style.opacity = 1;
+        // }
+    })
+}
