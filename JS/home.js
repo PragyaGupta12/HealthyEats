460ceb26513b735ba01f5df4525ac67b60c3b345
Pragya Gupta <pragya.gupta012@gmail.com>
2023-07-17 23:05:08 +0530

    modified

--- JS/home.js
+++ JS/home.js
@@ -1,3 +1,6 @@
 window.onload = function () {
-    document.getElementById("user").innerHTML = sessionStorage.getItem("username");
+    // document.getElementById("user").innerHTML = sessionStorage.getItem("username");
+    let text = localStorage.getItem("userdata");
+    let obj = JSON.parse(text);
+    document.getElementById("user").innerHTML = obj.name;
 }
--- JS/userData.js
+++ JS/userData.js
@@ -1,23 +1,28 @@
 window.onload = function () {
 
-    const loginForm = document.getElementById("login-form");
+    // const loginForm = document.getElementById("login-form");
     const loginButton = document.getElementById("form-btn");
     // const loginErrorMsg = document.getElementById("login-error-msg");
 
     loginButton.addEventListener("click", (e) => {
 
         // e.preventDefault();
-        const name = loginForm.username.value;
-        const pass = loginForm.password.value;
-        if (name !== '' && pass !== '' && pass) {
-            sessionStorage.setItem("username", name);
-            sessionStorage.setItem("password", pass);
-        }
+        // const name = loginForm.username.value;
+        // const pass = loginForm.password.value;
+        // if (name !== '' && pass !== '' && pass) {
+        //     sessionStorage.setItem("username", name);
+        //     sessionStorage.setItem("password", pass);
+        // }
 
         // if (name === sessionStorage.getItem("username", name) && pass === sessionStorage.getItem("password", pass)) {
         //     alert("You have successfully logged in.");
         // } // else {
         //     loginErrorMsg.style.opacity = 1;
         // }
+        const username = document.getElementById('form-username').value
+        const password = document.getElementById('form-password').value
+        const obj = { name: username, pass: password}
+        const JSONObj = JSON.stringify(obj)
+        localStorage.setItem("userdata", JSONObj);
     })
 }
